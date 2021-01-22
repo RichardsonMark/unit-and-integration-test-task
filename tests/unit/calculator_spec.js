fee6780dc94f848bf2aaa8890da84038a3af9efc
Mark Richardson <mark.richardson4@gmail.com>
2021-01-22 18:53:54 +0000

    clearClick (clear running total without affecting calculation) unit test added and passing

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -12,41 +12,49 @@ describe('calculator', function () {
   })
 
   // write unit tests here in the form of "it should do something..."
-  it('it can add 1 to 4 and get 5', function(){
+  it('can add 1 to 4 and get 5', function(){
     calculator.previousTotal = 1;
     calculator.add(4);
     assert.strictEqual(calculator.runningTotal, 5);
   })
 
-  it('it can subtract 4 from 7 and get 3', function(){
+  it('can subtract 4 from 7 and get 3', function(){
     calculator.previousTotal = 7;
     calculator.subtract(4);
     assert.strictEqual(calculator.runningTotal, 3);
   })
 
-  it('it can multiply 3 by 5 and get 15', function(){
+  it('can multiply 3 by 5 and get 15', function(){
     calculator.previousTotal = 3;
     calculator.multiply(5);
     assert.strictEqual(calculator.runningTotal, 15);
   })
 
-  it('it can divide 21 by 7 and get 3', function(){
+  it('can divide 21 by 7 and get 3', function(){
     calculator.previousTotal = 21;
     calculator.divide(7);
     assert.strictEqual(calculator.runningTotal, 3);
   })
 
-  it('it can take/concatenate multiple button clicks', function(){
+  it('can take/concatenate multiple button clicks', function(){
     calculator.previousTotal = 1;
     calculator.numberClick(75);
     assert.strictEqual(calculator.runningTotal, 75);
   })
 
-  it('it can chain multiple operations together', function(){
+  it('can chain multiple operations together', function(){
     calculator.operatorClick('+');
     assert.strictEqual(calculator.previousTotal, 0);
     assert.strictEqual(calculator.newTotal, true);
 
+  })
+
+  it('can clear the running total without affecting the calculation', function(){
+    calculator.previousTotal = 1;
+    calculator.numberClick(75);
+    calculator.clearClick();
+    assert.strictEqual(calculator.previousTotal, 1);
+    assert.strictEqual(calculator.runningTotal, 0);
   })
 
 });
